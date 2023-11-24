6b2e65a3050a1693f03544dc50051738906a8255
Justin Anderson <justinandersun@gmail.com>
2023-11-24 17:04:26 -0500

    analytics and html attributes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: "Justin Andersun",
-    siteUrl: `https://andersun.com`
+    description: "The personal blog of Justin Andersun, product manager and fiction writer.",
+    siteUrl: `https://andersun.com`,
+    tags: "product manager, speculative fiction writer, indie maker, highpointer",
+    twitterUsername: "@justinandersun",
   },
   plugins: [
     "gatsby-plugin-image",
@@ -14,5 +17,30 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
+    // HTML Attributes
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en'
+      }
+    },
+    // Google Analytics
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-GGPS849D7N",
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
   ],
 };
