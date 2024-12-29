9565b9412c318b5ba86050166c3ed9016fe04f03
Justin Anderson <justinandersun@gmail.com>
2024-12-28 20:44:19 -0600

    Jan 2025 Now and About update

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About">
       <h2>About me</h2>
-      <p>I am a husband, brother, son, grandson, friend, cousin, nephew, product manager, writer, and maker - in that order.</p>
+      <p>I am a husband, brother, son, grandson, friend, cousin, nephew, uncle, product manager, writer, and maker - in that order.</p>
 
       <p>I build products that prevent identity fraud.</p>
       
@@ -26,27 +26,27 @@ const AboutPage = () => {
             
       <p>Among the Big Five personality traits, I am:</p>
       <ul>
-        <li>Slightly more introverted than extroverted</li>
-        <li>Highly conscientious and obsessively organized</li>
-        <li>Pretty agreeable (becoming less so with age)</li>
-        <li>Highly neurotic and moody</li>
-        <li>Moderately open to new experiences</li>
+        <li>Slightly more introverted than extroverted.</li>
+        <li>Highly conscientious and obsessively organized.</li>
+        <li>Fairly agreeable (becoming less so with age).</li>
+        <li>Highly neurotic and moody.</li>
+        <li>Thrill-seeking and open to new experiences.</li>
       </ul>
       
       <p>Some other things about me:</p>
       <ul>
-        <li>I love long, meandering walks and conversations</li>
-        <li>I wear shoes even after they get holes in them</li>
-        <li>I laugh easily (and often uncontrollably)</li>
-        <li>I enjoy sports like hiking, biking, climbing, and skiing</li>
-        <li>I donate blood twice per year</li>
-        <li>I am Catholic</li>
-        <li>I am from Michigan and a proud Wolverine</li>
-        <li>I am a night owl who wishes to be a morning bird</li>
-        <li>I film <a href="https://www.youtube.com/user/justinandersun" target="_blank" rel="noreferrer">one-second daily videos</a></li>
-        <li>I can unicycle and juggle but am better at the former</li>
+        <li>I love long, meandering walks and conversations.</li>
+        <li>I wear shoes even after they get holes in them.</li>
+        <li>I laugh easily (and often uncontrollably).</li>
+        <li>I enjoy mountaineering, hiking, biking, climbing, skiing, and camping.</li>
+        <li>I donate blood twice per year.</li>
+        <li>I am Catholic.</li>
+        <li>I am from Michigan and a proud Wolverine.</li>
+        <li>I am a night owl who wishes to be a morning bird.</li>
+        <li>I film <a href="https://www.youtube.com/user/justinandersun" target="_blank" rel="noreferrer">one-second daily videos</a>.</li>
+        <li>I can unicycle and juggle but am better at the former.</li>
       </ul>
-      <p>Find me on <a href="https://x.com/justinandersun" target="_blank" rel="noreferrer">Twitter</a>, <a href="https://github.com/justinandersun/" target="_blank" rel="noreferrer">GitHub</a>, and <a href="https://www.linkedin.com/in/justinandersun/" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
+      <p>Find me on <a href="https://x.com/justinandersun" target="_blank" rel="noreferrer">X</a>, <a href="https://github.com/justinandersun/" target="_blank" rel="noreferrer">GitHub</a>, and <a href="https://www.linkedin.com/in/justinandersun/" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
     </Layout>
   )
 }
--- src/pages/now.js
+++ src/pages/now.js
@@ -5,13 +5,13 @@ import Seo from '../components/seo'
 const NowPage = () => {
   return (
     <Layout pageTitle="Now">
-      <p>As of October 2024, I am:</p>
+      <p>As of January 2025, I am:</p>
       <ul>
-        <li>Living in Minnesota</li>
-        <li>Leading a product team at a credit card company</li>
-        <li>Climbing the <a href="https://highpoint.guide/" target="_blank" rel="noreferrer">U.S. Highpoints</a> (I've summitted 48 of 50)</li>
-        <li>Writing speculative fiction stories</li>
-        <li>Reading literature and philosophy from the Western canon</li>
+        <li>Living in Minnesota.</li>
+        <li>Leading a product team at a credit card company.</li>
+        <li>Climbing the <a href="https://highpoint.guide/" target="_blank" rel="noreferrer">U.S. Highpoints</a> (I've summitted 48 of 50).</li>
+        <li>Publishing a monthly newsletter about philosophy & mythology on <a href="https://turtlespace.blog/" target="_blank" rel="noreferrer">Turtle's Pace</a>.</li>
+        <li>Writing a novel about the Underworld.</li>
       </ul>
       <p>This is a <a href="https://nownownow.com/about" target="_blank" rel="noreferrer">now page</a>, and you can create one too.</p>
     </Layout>
