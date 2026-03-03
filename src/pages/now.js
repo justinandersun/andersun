39490765f81d0350e0bcfc44a5b04a238ded2245
Justin Andersun <justinandersun@gmail.com>
2026-03-03 00:14:53 -0600

    March 2026 now updates

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,12 +6,13 @@ const NowPage = () => {
   return (
     <Layout pageTitle="Now">
     <h2>Now</h2>
-      <p>As of February 2026, I am:</p>
+      <p>As of March 2026, I am:</p>
       <ul>
+        <li>Learning archery</li>
+        <li>Participating in Exodus 90, a season of prayer and asceticism</li>
         <li>Leading a product team at Mastercard</li>
         <li>Writing the first draft of a novel about the Underworld</li>
         <li>Facilitating the <a href="https://www.inkyloons.com/" target="_blank" rel="noreferrer">Inky Loons</a> writing group in Minneapolis</li>
-        <li>Learning archery</li>
         <li>Procrastinating my final <a href="https://highpoint.guide/" target="_blank" rel="noreferrer">U.S. Highpoint</a>: Denali</li>
       </ul>
       <p>This is a <a href="https://nownownow.com/about" target="_blank" rel="noreferrer">now page</a>, and you can create one too.</p>
