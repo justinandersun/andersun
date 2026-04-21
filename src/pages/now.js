d3610021344a0a6dba925b68a638cd7f31eb6346
Justin Andersun <justinandersun@gmail.com>
2026-04-21 15:06:45 -0500

    April 2026 updates

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,11 +6,10 @@ const NowPage = () => {
   return (
     <Layout pageTitle="Now">
     <h2>Now</h2>
-      <p>As of March 2026, I am:</p>
+      <p>As of April 2026, I am:</p>
       <ul>
-        <li>Observing Exodus 90, a season of prayer and asceticism</li>
-        <li>Leading a product team at Mastercard</li>
-        <li>Learning archery</li>
+        <li>Building <a href="https://www.stu.tools/" target="_blank" rel="noreferrer">Stu</a>, an OS for your home</li>
+        <li><s>Leading a product team at Mastercard</s></li>
         <li>Writing the first draft of a novel about the Underworld</li>
         <li>Facilitating the <a href="https://www.inkyloons.com/" target="_blank" rel="noreferrer">Inky Loons</a> writing group in Minneapolis</li>
         <li>Procrastinating my final <a href="https://highpoint.guide/" target="_blank" rel="noreferrer">U.S. Highpoint</a>: Denali</li>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,13 +6,14 @@ const ProjectPage = () => {
   return (
     <Layout pageTitle="Projects">
     <h2>Projects</h2>
-     <p>I make clean, ad-free products that serve niche communities.</p>
+     <p>I make clean, ad-free products that help people solve problems and achieve their goals.</p>
 
-      <p><a href="https://highpoint.guide/" target="_blank" rel="noreferrer">Highpoint Guide</a><br /><i>A manual for climbing the U.S. highpoints</i></p>
-      <p><a href="https://productfield.guide/" target="_blank" rel="noreferrer">Product Field Guide</a><br /><i>Essays on product management</i></p>
-      <p><a href="https://bodyweight.fit/" target="_blank" rel="noreferrer">Bodyweight Fit</a><br /><i>A generator of bodyweight workout routines</i></p>
-      <p><a href="https://model.garden/" target="_blank" rel="noreferrer">Model Garden</a><br /><i>A collection of mental models</i></p>
-      <p><a href="https://inkyloons.com" target="_blank" rel="noreferrer">Inky Loons</a><br /><i>A Minneapolis-based writing group</i></p>
+    <p><a href="https://stu.tools/" target="_blank" rel="noreferrer">Stu</a><br /><i>An OS for your home</i></p>
+    <p><a href="https://highpoint.guide/" target="_blank" rel="noreferrer">Highpoint Guide</a><br /><i>A manual for climbing the U.S. highpoints</i></p>
+    <p><a href="https://productfield.guide/" target="_blank" rel="noreferrer">Product Field Guide</a><br /><i>Essays on product management</i></p>
+    <p><a href="https://bodyweight.fit/" target="_blank" rel="noreferrer">Bodyweight Fit</a><br /><i>A generator of bodyweight workout routines</i></p>
+    <p><a href="https://model.garden/" target="_blank" rel="noreferrer">Model Garden</a><br /><i>A collection of mental models</i></p>
+    <p><a href="https://inkyloons.com" target="_blank" rel="noreferrer">Inky Loons</a><br /><i>A Minneapolis-based writing group</i></p>
     </Layout>
   )
 }
