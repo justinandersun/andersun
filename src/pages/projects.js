286f670a31f5055b2919f1ce9840ebc9459bb7c6
Justin Andersun <justinandersun@gmail.com>
2026-04-21 15:52:29 -0500

    Automate this project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,6 +12,7 @@ const ProjectPage = () => {
     <p><a href="https://highpoint.guide/" target="_blank" rel="noreferrer">Highpoint Guide</a><br /><i>A manual for climbing the U.S. highpoints</i></p>
     <p><a href="https://productfield.guide/" target="_blank" rel="noreferrer">Product Field Guide</a><br /><i>Essays on product management</i></p>
     <p><a href="https://bodyweight.fit/" target="_blank" rel="noreferrer">Bodyweight Fit</a><br /><i>A generator of bodyweight workout routines</i></p>
+    <p><a href="https://automate-this.vercel.app/" target="_blank" rel="noreferrer">Should I Automate This?</a><br /><i>A simple calculator to answer this question</i></p>
     <p><a href="https://model.garden/" target="_blank" rel="noreferrer">Model Garden</a><br /><i>A collection of mental models</i></p>
     <p><a href="https://inkyloons.com" target="_blank" rel="noreferrer">Inky Loons</a><br /><i>A Minneapolis-based writing group</i></p>
     </Layout>
